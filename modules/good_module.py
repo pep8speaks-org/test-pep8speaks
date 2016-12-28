9a5451618a213161a8015ef60cdc2d7468efc450
Himanshu Mishra <himanshu2014iit@gmail.com>
2016-12-28 15:51:39 +0530

    Add a pep8 correct module

---
+++ modules/good_module.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+
+"""Receive request to show notification"""
+import ast
+import requests
+import sys
+import subprocess
+
+from ping_me.utils import cryptex
+import ping_me.authenticate
+
+
+def main():
+    """Executed by cron every minute. Sends POST request to recieve
+    reminder for upcoming minute."""
+
+    target = "http://ping-me.himanshumishra.in/ping/"
+    email = ping_me.authenticate.extract_email()
+    key = ping_me.authenticate.extract_password()
+    data_t = {
+        "email": email,
+        "password": key
+    }
+    r = requests.post(target, data=data_t)
+    if ast.literal_eval(r.text)["success"] == "True":
+        message = cryptex.decryptor(key, ast.literal_eval(r.text)["message"])
+        if sys.platform == 'linux2':
+            subprocess.call(['notify-send', message])
+        elif sys.platform == 'darwin':
+            # Need to install `terminal-notifier`
+            # $ brew install terminal-notifier
+            subprocess.call(['terminal-notifier', '-title',
+                             'ping-me', message])
+        elif sys.platform in ['win32', 'win64']:
+            # Do things for windows
+            pass
+
+if __name__ == '__main__':
+    main()
