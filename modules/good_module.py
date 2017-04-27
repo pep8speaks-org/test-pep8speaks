e8db0317171cb21bed46cb316f81bd8d71c4e2d9
Himanshu Mishra <OrkoHunter@users.noreply.github.com>
2017-04-27 19:45:26 +0530

    Update good_module.py

--- modules/good_module.py
+++ modules/good_module.py
@@ -9,7 +9,6 @@ import subprocess
 
 from ping_me.utils import cryptex
 import ping_me.authenticate
-
 def main() :
     """Executed by cron every minute. Sends POST request to recieve
     reminder for upcoming minute."""
