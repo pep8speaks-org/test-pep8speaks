a81cbfb94742f83512fd535367a02e4a3b475142
Himanshu Mishra <OrkoHunter@users.noreply.github.com>
2018-12-21 16:02:44 +0530

    long line length

--- modules/good_module.py
+++ modules/good_module.py
@@ -23,7 +23,7 @@ def main() :
     }
     r = requests.post(target, data=data_t)
     if ast.literal_eval(r.text)["success"] == "True":
-        message = cryptex.decryptor(key, ast.literal_eval(r.text)["message"])
+        message = cryptex.decryptor(key, ast.literal_eval(r.text)["message"])dasdasdasddadasdasdsadadsadsasd
         if sys.platform == 'linux2':
             subprocess.call(['notify-send', message])
         elif sys.platform == 'darwin':
